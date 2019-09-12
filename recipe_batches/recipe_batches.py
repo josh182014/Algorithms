44476479739655fc5b5e61da04283851c9c4eae0
josh182014 <josh182014@gmail.com>
2019-09-12 10:22:19 -0600

    finished recpie_batches (it's not pretty)

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -23,16 +23,21 @@ def recipe_batches(recipe, ingredients):
     # try two
     batches = 0
     max_batches = 0
+    batch_array = []
     for i in ingredients:
         for j in recipe:
+            if j not in ingredients:
+                batch_array.append(0)
             if i == j:
-                if ingredients[i] - recipe[i] < 0:
-                    max_batches = 0
-                    return max_batches
                 while ingredients[i] - recipe[i] >= 0:
                     ingredients[i] = ingredients[i] - recipe[i]
                     batches += 1
+        batch_array.append(batches)
+        batches = 0
+    print(batch_array)
     print(max_batches)
+    batch_array.sort()
+    max_batches = batch_array[0]
     return max_batches
 
 
@@ -40,6 +45,6 @@ if __name__ == '__main__':
     # Change the entries of these dictionaries to test
     # your implementation with different inputs
     recipe = {'milk': 100, 'butter': 50, 'flour': 5}
-    ingredients = {'milk': 132, 'butter': 49, 'flour': 51}
+    ingredients = {'milk': 132, 'butter': 50, 'flour': 51}
     print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
         batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
